fe330bdc288bff203a94d38e4d1fae96ff8082d5
ol97 <olejnik.lukasz97@gmail.com>
2020-11-25 20:18:20 +0100

    RECYCLER VIEW AND NAV TO DETAIL VIEW IS WORKING

--- app/src/main/java/com/example/yogaapp/MyItemRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/yogaapp/MyItemRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.yogaapp
 
+import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
@@ -26,16 +27,30 @@ class MyItemRecyclerViewAdapter(
         holder.textViewName.text = item[1]
         holder.textViewDate.text = item[2]
         holder.textViewHour.text = item[3]
+        holder.id = item[0]
 
-        val id = item[0]
     }
 
     override fun getItemCount(): Int = values.size
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
         val textViewDate: TextView = view.findViewById(R.id.textViewDate)
         val textViewHour: TextView = view.findViewById(R.id.textViewHour)
         val textViewName: TextView = view.findViewById(R.id.textViewName)
+        public lateinit var id: String
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val navController = v?.let { Navigation.findNavController(it) }
+
+            if (navController != null) {
+                navController.navigate(ItemListFragmentDirections.actionItemListFragmentToSessionDetailsFragment(id))
+            }
+            Log.d("AAA", "123")
+        }
 
     }
 }
